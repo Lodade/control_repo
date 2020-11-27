4bd351d81a7b13d5a58b89b7bdd6af10a990a12e
Jacob Wagner <37224070+Lodade@users.noreply.github.com>
2020-11-26 20:00:37 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,3 @@
 node default {
-  file {'/root/README':
-    ensure => file,
-    content => "This is a README",
-    owner => "root",
-  }
-  file {'/root/testing.txt':
-    ensure => present,
-    content => "I appeared :)",
-  }
+  
 }
