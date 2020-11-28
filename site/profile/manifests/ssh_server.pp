a6b034c9f515185865181d2430a783b18452f207
Jacob Wagner <37224070+Lodade@users.noreply.github.com>
2020-11-27 23:23:06 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCXHLCWsDlcigkN4mo0AKfE02iyGeNo9bjckkqxq3LGuQZ8vGRPayLLxwVEEoo8gFBgT4x6M5OjxFLZqqLU9PGX7ct6VvvH5LZFtMlREog2tCQ+G5yLf8rbzOvSOSs51jIvGIFSMSNGDZLcxKZhGJxCsL78gh+ltDk2ArrOeYwNqNl/McBhrvxX/BvKS1p98WFLZgMETPcvIh+B7eHgXTkgzmDd+CiH2/UNvJJFZ10TqyzWBJgOQfzrzbs5C4LHInw6N7mP55K8HBT9CE9hQ4ZkJkAOSIc/V+hXafy5dW4powQ0rl0awN7Lm//EJ0XTxAEH7xcki0F5kMdF1IdfGHtn',
 	}  
 }
